65fe890fd385cc816fba6463f5984a0e946fe00d
seunggeon <tmdrjs9555@naver.com>
2024-08-12 20:33:39 +0900

    flag를 통한 config path 관리

--- init/cmd/cmd.go
+++ init/cmd/cmd.go
@@ -1,10 +1,20 @@
 package cmd
 
+import (
+	"awesomeProject/config"
+	"fmt"
+)
+
 type Cmd struct {
+	config *config.Config
 }
 
-func NewCmd() *Cmd {
-	c := &Cmd{}
+func NewCmd(filePath string) *Cmd {
+	c := &Cmd{
+		config: config.NewConfig(filePath),
+	}
+
+	fmt.Println(c.config.Server.Port)
 
 	return c
 }
--- init/main.go
+++ init/main.go
@@ -1,19 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"awesomeProject/init/cmd"
+	"flag"
 )
 
+var configPathFlag = flag.String("config", "/.config.toml", "config file not found")
+
 func main() {
-	http.HandleFunc("/", helloWorld)
-
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Println("에러가 떴네용!")
-		panic(err)
-	}
-}
-
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("hello world")
+	flag.Parse()
+	cmd.NewCmd("./config.toml")
 }
