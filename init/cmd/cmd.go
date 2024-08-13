37024a313f90cefb32aa38b753360f09c8a30379
seunggeon <tmdrjs9555@naver.com>
2024-08-13 19:18:22 +0900

    feat: add service & repository layer

--- init/cmd/cmd.go
+++ init/cmd/cmd.go
@@ -3,18 +3,25 @@ package cmd
 import (
 	"awesomeProject/config"
 	"awesomeProject/network"
+	"awesomeProject/repository"
+	"awesomeProject/service"
 )
 
 type Cmd struct {
-	config  *config.Config
-	network *network.Network
+	config     *config.Config
+	network    *network.Network
+	service    *service.Service
+	repository *repository.Repository
 }
 
 func NewCmd(filePath string) *Cmd {
 	c := &Cmd{
-		config:  config.NewConfig(filePath),
-		network: network.NewNetwork(),
+		config: config.NewConfig(filePath),
 	}
+
+	c.repository = repository.NewRepository()
+	c.service = service.NewService(c.repository)
+	c.network = network.NewNetwork(c.service)
 
 	c.network.ServerStart(c.config.Server.Port)
 
